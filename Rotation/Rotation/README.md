789e66f11bce09c28e2e7d88f243d5e7d3c28740
YoungJae <dudwo8075@gmail.com>
2021-09-23 17:33:16 +0900

    initial commit

---
+++ Rotation/Rotation/README.md
@@ -0,0 +1,4 @@
+## Swift5 N x M 크기의 2차원 배열 생성 방법
+<pre>
+    let array: [[Int]] = Array(repeating: Array(repeating: 0, count: m), count: n)
+</pre>
---
+++ Rotation/Rotation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Rotation
+//
+//  Created by 이영재 on 2021/09/23.
+//
+
+import Foundation
+
+let array: Array<Array<Int>> = Array(repeating: Array(repeating: 0, count: 3), count: 5)
+print(array)
+
